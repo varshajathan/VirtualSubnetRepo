5582e38f24efcedbb88f7adad8152d953ac06ed5
varshajathan <48399281+varshajathan@users.noreply.github.com>
2025-04-18 11:40:53 +0530

    Update routes.tf

--- routes.tf
+++ routes.tf
@@ -12,10 +12,11 @@ resource "google_compute_router_nat" "nat_config" {
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
 
-resource "google_compute_route" "default-internet" {
-  name           = "default-internet-route"
-  network        = google_compute_network.custom_vpc.id
-  destination_range = "0.0.0.0/0"
-  next_hop_internet = true
-  priority        = 1000
+resource "google_compute_route" "default_internet" {
+  name        = "default-internet-route"
+  network     = google_compute_network.custom_vpc.name
+  dest_range  = "0.0.0.0/0"              # ✅ Correct attribute
+  next_hop_gateway = "default-internet-gateway"  # ✅ Correct for default IGW
+  priority    = 1000
 }
+
